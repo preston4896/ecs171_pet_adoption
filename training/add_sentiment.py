87b73000aa46c844329a528097f5962f8f63fb55
Xuan Deng <xudeng@ucdavis.edu>
2019-11-13 14:33:11 -0800

    Used average magnitude and score for pets missing csv file

--- training/add_sentiment.py
+++ training/add_sentiment.py
@@ -8,18 +8,29 @@ zeros = np.zeros((len(data), 2), dtype=float)
 data = np.concatenate((data, zeros), 1)
 
 fileNotFound = 0
+fileMissingIndex = []
+magnitude = 0
+score = 0
 for i in range(0, len(data)):
 	try:
 		fileName = 'train_sentiment/' + petID[i][0] + '.json'
 		with open(fileName) as json_file:
 		    info = json.load(json_file)
-		    data[i, -1] = info['documentSentiment']['magnitude']
-		    data[i, -2] = info['documentSentiment']['score']
+		    data[i, -2] = info['documentSentiment']['magnitude']
+		    data[i, -1] = info['documentSentiment']['score']
+		    magnitude+=info['documentSentiment']['magnitude']
+		    score+=info['documentSentiment']['score']
 	except:
+		fileMissingIndex.append(i)
 		fileNotFound += 1
 
 		# uncomment to see which files are missing
 		#print("Could not find {}.json".format(petID[i][0]))
+		
+averageMagnitude = magnitude / (len(data) - fileNotFound)
+averageScore = score / (len(data) - fileNotFound)
 
+data[fileMissingIndex, -2] = averageMagnitude
+data[fileMissingIndex, -1] = averageScore
 print("Could not find {} files".format(fileNotFound))
 data = np.save('data', data, allow_pickle=True)
