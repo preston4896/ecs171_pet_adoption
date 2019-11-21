90385482beb11bdbd5a2dcb2485dccc886d64047
Gareth-Yao <347747741@qq.com>
2019-11-21 14:40:58 -0800

    turn to test_script

--- training/create_neural_network.py
+++ training/create_neural_network.py
@@ -3,12 +3,20 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.optimizers import Adam
 from get_data_2 import *
-def create_network(lr=0.0066,num_Nodes=18, dropout=0.3,final_activation='sigmoid', loss='categorical_crossentropy',n=n):
+
+
+# Hpyerparameter Optimization:
+# round 1: lr=0.0033,num_Nodes=12,dropout=1,loss='mean_Squared_error'
+# round 2: lr=0.0066,num_Nodes=18, dropout=0.3,final_activation='sigmoid', loss='categorical_crossentropy'
+# round 3:
+def create_network(lr=0.0066, num_Nodes=18, dropout=0.3, final_activation='sigmoid',
+                   loss_function='categorical_crossentropy', n=n):
     model = Sequential()
     model.add(Dense(num_Nodes, input_dim=n, activation='sigmoid', kernel_initializer='random_uniform'))
     model.add(Dropout(dropout))
     model.add(Dense(num_Nodes, activation='sigmoid', kernel_initializer='random_uniform'))
     model.add(Dropout(dropout))
     model.add(Dense(5, activation=final_activation, kernel_initializer='random_uniform'))
-    model.compile(optimizer=Adam(learning_rate=lr), loss=loss, metrics=['accuracy'])
+    model.compile(optimizer=Adam(lr=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-8), loss=loss_function,
+                  metrics=['accuracy'])
     return model
--- training/training_model.py
+++ training/training_model.py
@@ -2,6 +2,7 @@ from get_data_2 import *
 import numpy as np
 from keras.callbacks import LambdaCallback
 from keras.wrappers.scikit_learn import KerasClassifier
+from keras.callbacks import EarlyStopping
 from sklearn.model_selection import cross_val_score
 from create_neural_network import create_network
 import matplotlib.pyplot as plt
@@ -16,16 +17,20 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 # Train
 
 model = create_network()
-epochs = 400
+epochs = 1000
 train_scores = []
 test_scores = []
-eval_acc = LambdaCallback(on_epoch_end=lambda batch, logs: print(model.evaluate(x_test, y_test)[1]))
 train_loss = LambdaCallback(on_epoch_end=lambda batch, logs: train_scores.append(logs['loss']))
 test_loss = LambdaCallback(on_epoch_end=lambda batch, logs: test_scores.append(logs['val_loss']))
-wrapper = KerasClassifier(build_fn=create_network, epochs=epochs,batch_size=32,verbose=2)
-model.fit(x_train, y_train, validation_split=0.2, epochs=epochs, batch_size=32, verbose=1, class_weight=None, callbacks=[train_loss,test_loss])
+wrapper = KerasClassifier(build_fn=create_network, epochs=epochs,batch_size=32,verbose=0)
+earlystopper = EarlyStopping(monitor='val_loss', patience=epochs/10)
+model.fit(x_train, y_train, validation_split=0.2, epochs=epochs, batch_size=32, verbose=1,
+          class_weight='Balanced', callbacks=[train_loss,test_loss,earlystopper])
 # Evaluate
-val_scores = cross_val_score(wrapper, x_train, y_train, cv=5)
+val_scores = cross_val_score(wrapper, x_train, y_train, cv=5, verbose=0, n_jobs=-1,
+                             fit_params={"callbacks":[EarlyStopping(monitor='loss', patience=epochs/10)]})
+model.fit(x_train, y_train, epochs=epochs, batch_size=32, verbose=0,
+          class_weight='Balanced', callbacks=[EarlyStopping(monitor='loss', patience=epochs/10)])
 print("testing accuracy:",model.evaluate(x_test, y_test)[1])
 print("cv accuracy:",np.mean(val_scores))
 
@@ -33,16 +38,16 @@ print("cv accuracy:",np.mean(val_scores))
 plt.title("Learning Curve")
 plt.grid()
 
-plt.fill_between(np.linspace(0,epochs,epochs), train_scores,
+plt.fill_between(np.linspace(0,len(train_scores),len(train_scores)), train_scores,
                   alpha=0.1, color="r")
-plt.fill_between(np.linspace(0,epochs,epochs), test_scores,
+plt.fill_between(np.linspace(0,len(test_scores),len(test_scores)), test_scores,
              alpha=0.1, color="g")
 plt.ylabel("loss")
 plt.xlabel("epochs")
 plt.ylim(top=max(train_scores),bottom=min(train_scores))
-plt.plot(np.linspace(0,epochs,epochs), train_scores, 'o-', color="r",
+plt.plot(np.linspace(0,len(train_scores),len(train_scores)), train_scores, 'o-', color="r",
          label="Training score")
-plt.plot(np.linspace(0,epochs,epochs), test_scores, 'o-', color="g",
+plt.plot(np.linspace(0,len(test_scores),len(test_scores)), test_scores, 'o-', color="g",
           label="validation score")
 legend = plt.legend(loc='upper right', shadow=True, fontsize='medium')
 legend.get_frame().set_facecolor('C0')
